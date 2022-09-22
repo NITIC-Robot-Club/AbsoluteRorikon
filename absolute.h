2fb0ca4ae0015a25c65ca3bb255fbf832b292f27
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-09-23 05:05:54 +0900

    fix: fix header file

--- absolute.h
+++ absolute.h
@@ -8,6 +8,7 @@ private:
   int save, value, result;
   DigitalIn data0, data1, data2, data3, data4, data5, data6, data7;
 
+public:
   absolute(PinName pin0, PinName pin1, PinName pin2, PinName pin3, PinName pin4,
            PinName pin5, PinName pin6, PinName pin7);
 
